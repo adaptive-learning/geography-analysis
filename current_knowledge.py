85373283303ceb75d779acaccd68ac691db7b93f
Radek <xpelanek@fi.muni.cz>
2014-02-10 11:09:49 +0100

    update of PFA based models; parameter tuning; bit hacked version

--- current_knowledge.py
+++ current_knowledge.py
@@ -4,14 +4,8 @@ from geography_common import *
 from models_current_knowledge import *
 import sys
 
-# outdated... now I prefer to use RMSE... maybe
-def sensi_analysis_roc(data, Model, values):
-    for pval in values:
-        print "par value", pval
-        m = Model(pval)
-        m.process_data(data)
-        do_roc(*zip(*m.log))
-    plt.show()
+## needs cleaning...
+## parameter search needs improving
 
 def sensi_analysis(data, Model, values, par_name = None, do_plot = 1, verbose=1, place = None):
     rmses = []
@@ -26,10 +20,7 @@ def sensi_analysis(data, Model, values, par_name = None, do_plot = 1, verbose=1,
             m = Model(**args)
         m.process_data(data, place)
         r = log_rmse(m.log) 
-        if verbose: print "\tRMSE", round(r,3)
-#        r = log_power4_error(m.log) 
-#        r = log_rmse2(m.log) 
-#        if verbose: print "\talternative er fun", round(r,3)
+        if verbose: print "\tRMSE", round(r,4)
         rmses.append(r)
         if rmses[i] < rmses[best]: best = i
     print "Best par value:", values[best]
@@ -51,18 +42,43 @@ def sensi_analysis_grid(data, Model, parameters):
     print "Best:", best
     print bestparam    
 
+def sensi_analysis_grid2d(data, Model, parameters):
+    logger = MultipleRunLogger(1)
+    pars = parameters[0].keys()
+    assert len(pars) == 2  # plus some hidden assumptions...
+    for param in parameters:
+        m = Model(**param)
+        m.process_data(data)
+        logger.log(`param[pars[0]]`, `param[pars[1]]`, log_rmse(m.log))
+    logger.print_table(print_best = 2)
+    plt.imshow(logger.get_table())
+    plt.show()
+    
 def sensi_analysis_gridPFA(data):    
     parameters = [ { "Kgood": Kg, "Kbad": Kb }
                    for Kg in [ 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4 ]
                    for Kb in [-0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6]
                   ]
-    sensi_analysis_grid(data, PFA, parameters)
+    sensi_analysis_grid(data, PFAbasic, parameters)
+
+def sensi_analysis_gridPFA2(data):    
+    # parameters = [ { "Kgood": Kg, "Kbad": Kb }
+    #                for Kg in [ 0.7, 0.8, 0.9, 1.0, 1.1 ]
+    #                for Kb in [ 0.3, 0.4, 0.5, 0.6, 0.7]
+    #               ]
+    # sensi_analysis_grid2d(data, PFAbasic, parameters)
+    parameters = [ { "Kgood": Kg, "Kbad": Kb }
+                   for Kg in [ 3.0, 3.2, 3.3, 3.4, 3.5 ]
+                   for Kb in [ -0.4, -0.3, -0.2, -0.1, 0]
+                  ]
+    sensi_analysis_grid2d(data, PFAplus, parameters)
+
     
 def sensi_analysis_per_place(data, Model, parameters):
     states, _ = process_states()
     D = read_dict("data/raschD.csv")
     for place in data:
-        if len(data[place].keys()) <  180: continue
+        if len(data[place].keys()) < 140: continue
         best = float("inf")        
         bestparam = None
         for param in parameters:
@@ -81,7 +97,7 @@ def PFA_sensi_analysis_per_place(data):
                    for Kg in [ 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4 ]
                    for Kb in [-0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6]
                   ]    
-    sensi_analysis_per_place(data, PFA, parameters)        
+    sensi_analysis_per_place(data, PFAbasic, parameters)        
 
 def Elo_sensi_analysis_per_place(data):
     parameters = [ { "alpha": a }
@@ -89,20 +105,29 @@ def Elo_sensi_analysis_per_place(data):
                   ]    
     sensi_analysis_per_place(data, Elo, parameters)        
 
+def PFAplus_sensi_analysis_per_place(data):
+    parameters = [ { "Kgood": Kg, "Kbad": Kb }
+                   for Kg in [ 2, 2.5, 3, 3.5, 4 ]
+                   for Kb in [ -0.2, 0, 0.2, 0.4, 0.6 ]
+                  ]    
+    sensi_analysis_per_place(data, PFAplus, parameters)            
     
-def compare_models(data, models):
+def compare_models(data, models, textable = 0, roccurves = 0):
     leg = []
     for m in models:
-        print str(m), " "*(max(1,20 - len(str(m)))),
         leg.append(str(m))
         m.process_data(data)
-#        do_roc(*zip(*m.log))
-        print "\tRMSE    \t", round(log_rmse(m.log), 3), 
-        print "\tRMSE2    \t", round(log_rmse2(m.log), 3), 
-        print "\tLL\t", round(log_logloss(m.log), 3),
-        print "\tAUC    \t", round(log_auc(m.log), 3)
-#     plt.legend(leg, loc = 4)
-#    plt.show()
+        if textable:
+            print str(m), "&", round(log_rmse(m.log), 3), "&", round(log_logloss(m.log), 3), "&", round(log_auc(m.log), 3), r"\\"
+        else:
+            print str(m).ljust(30),
+            print "\tRMSE    \t", round(log_rmse(m.log), 3), 
+            print "\tLL\t", round(log_logloss(m.log), 3),
+            print "\tAUC    \t", round(log_auc(m.log), 3)
+            if roccurves: do_roc(*zip(*m.log))
+    if roccurves:
+        plt.legend(leg, loc = 4)
+        plt.show()
 
 def show_predictions(data, m1, m2):
     m1.process_data(data)
@@ -151,16 +176,19 @@ def show_prediction_classes(data, models):
 
 def run_sensi_analysis(data, choices):
     options = {
-        'BKT': [ BKT, [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6, 0.7 ] ],
+        'BKT': [ BKT, [0.2, 0.3, 0.4, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8 ] ],
+        'BKTs': [ BKT, [0.03, 0.04, 0.05,  0.06, 0.07,  0.08, 0.09 ], "pslip" ],
+        'BKTg': [ BKT, [ 0.05,  0.1, 0.12, 0.15, 0.2, 0.25 ], "pguess10" ],
         'decay': [ TimeDecay, [0, 0.05, 0.1, 0.2, 0.3, 0.4 ] ],
         'Elo': [ EloBasic, [0.75, 1.0,  1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75] ],
         'Elot': [ Elo, [ 0, 40, 80, 120, 160, 200, 240 ], "time_effect" ],
         'EloRt': [ Elo, [ 0, 0.2, 0.4, 0.6, 0.8, 1.0 ], "rtime_effect" ],
         'Eloa': [ Elo, [ 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 4.0 ], "alpha" ],
         'PFAg': [ PFA, [ 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6], "Kgood"],
-        'PFAElog': [ PFAElo, [ 2, 2.5, 3, 3.5, 4, 5], "Kgood"],
-        'PFAElob': [ PFAElo, [ 0, 0.2, 0.4, 0.6, 0.8, 1.2], "Kbad"],
-        'PFAElot': [ PFAElo, [ 0, 40, 80, 120, 160, 200, 240 ], "time_effect" ],
+        'PFA+g': [ PFAplus, [ 2, 2.5, 3, 3.5, 4, 5], "Kgood"],
+        'PFA+b': [ PFAplus, [ -0.6, -0.4, -0.3, -0.2, -0.1, 0, 0.2 ], "Kbad"],
+        'PFA+t': [ PFAplus, [ 0, 40, 80, 120, 160, 200, 240 ], "time_effect" ],
+        'PFA+r': [ PFAplus, [ 0, 0.1, 0.2, 0.3, 0.4 ],  "rtime_effect" ],
         'PFAb': [ PFA, [ -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0], "Kbad"],
         'PFAt': [ PFA, [ 0, 40, 60, 80, 120, 160 ], "time_effect"],
         'PFArt': [ PFA, [ -0.4, -0.2, 0, 0.2, 0.4, 0.6  ], "rtime_effect"]
@@ -175,12 +203,21 @@ def run_sensi_analysis(data, choices):
         else: place = None
         sensi_analysis(data, *options[choice], place=place)
 
+def run_sensi_analysis2(data, model, param):
+    modelmap = { 'PFAbasic': PFAbasic, 'PFAtime': PFAtime,
+                 'PFAprob': PFAprob, 'PFAplus': PFAplus }
+    paramlist = {
+#        'Kgood': [  0.6, 0.7, 0.75, 0.8, 0.9, 1.0, 1.1, 1.2],
+        'Kgood': [  2.8, 2.9, 3, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6], # hack
+        'Kbad': [ -0.6, -0.4, -0.3, -0.2, -0.1, 0, 0.2, 0.4, 0.5, 0.6, 0.8],
+        'time_effect': [ 20, 40, 60, 80, 100, 120, 160, 200, 240 ],
+        'rtime_effect': [ 0, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4 ] }
+    sensi_analysis(data, modelmap[model], paramlist[param], param)
+    
+
 def run_model_comparison(data, choices):
     options = {
-        '0': [ BKT(), Elo(), PFA(), PFAElo() ],
-        '1': [ BKT(), Elo(), PFA(), TimeDecay() ],
-        '2': [ BKT(), Elo(time_effect=0), PFA(time_effect=0), PFAElo(time_effect=0) ],
-        '8': [ BKT(0.3), Elo(0.3,100), PFA() ],
+        '0': [ BKT(), PFAbasic(), PFAtime(), PFAprob(), PFAplus()  ]
         }
     if len(choices) == 0 or not choices[0] in options:
         choices = [ '0' ]
@@ -194,10 +231,16 @@ def main():
     data = read_combined_data(datafile)
     if sys.argv[1] == "sensi":
         run_sensi_analysis(data, sys.argv[2:]) 
+    elif sys.argv[1] == "sensi2":
+        run_sensi_analysis2(data, *sys.argv[2:]) 
     elif sys.argv[1] == "gridPFA":
         sensi_analysis_gridPFA(data) 
+    elif sys.argv[1] == "grid2":
+        sensi_analysis_gridPFA2(data) 
     elif sys.argv[1] == "placesPFA":
         PFA_sensi_analysis_per_place(data) 
+    elif sys.argv[1] == "placesPFA+":
+        PFAplus_sensi_analysis_per_place(data)        
     elif sys.argv[1] == "placesElo":
         Elo_sensi_analysis_per_place(data) 
     elif sys.argv[1] == "compare":
@@ -206,10 +249,22 @@ def main():
 #        show_predictions(data, PFA(), Elo()  )        
         show_predictions(data, Elo(), Elo(rtime_effect = 0.5)  )        
     elif sys.argv[1] == "classes":
-        show_prediction_classes(data, [ PFA(), Elo(), BKT()])
+        show_prediction_classes(data, [ PFA(), Elo(), PFAplus(), BKT()])
     else:
         print "Dont know what", sys.argv[1], "means."
     
 if __name__ == "__main__":
     main()
         
+############################ outdated
+########## for the moment ...
+    
+# outdated... now I prefer to use RMSE... maybe
+def sensi_analysis_roc(data, Model, values):
+    for pval in values:
+        print "par value", pval
+        m = Model(pval)
+        m.process_data(data)
+        do_roc(*zip(*m.log))
+    plt.show()
+    
--- models_current_knowledge.py
+++ models_current_knowledge.py
@@ -45,62 +45,41 @@ class ModelC:
 
 class BKT(ModelC):
 
-    def __init__(self, plearn = 0.6):
+    def __init__(self, plearn = 0.6, pslip = 0.06, pguess10 = 0.12):
         self.plearn = plearn
+        self.pslip = pslip
+        self.pguess10 = pguess10
 
     def __str__(self):
-        return "BKT " + `self.plearn` 
-        
+        return "BKT " + `self.plearn` + ' ' + `self.pslip`
+
+    def guess(self, qtype):
+        if qtype == 10: return self.pguess10
+        return 1.0 / (qtype - 10*(qtype//10))
+    
     def process_sequence(self, initskill, ans, qtype, time, rtime):
         P = sigmoid(initskill)
         for i in range(len(ans)):
             qt = qtype[i]
-            pred = P*(1-slip(qt)) + (1-P)*guess(qt)
+            pred = P*(1-self.pslip) + (1-P)*self.guess(qt)
             if i > 0: self.save_to_log(pred, ans[i])
             if ans[i] == 1:
-                P = P*(1-slip(qt)) / (P*(1-slip(qt)) + (1-P)*guess(qt))
+                P = P*(1-self.pslip) / (P*(1-self.pslip) + (1-P)*self.guess(qt))
             else:
-                P = P*slip(qt) / (P*slip(qt) + (1-P)*(1-guess(qt)))
+                P = P*self.pslip / (P*self.pslip + (1-P)*(1-self.guess(qt)))
             P = P + (1-P) * self.plearn
-                        
-class Elo(ModelC):
 
-    def __init__(self, alpha = 2.0, time_effect = 160.0, rtime_effect = 0):
-        self.alpha = alpha
-        self.time_effect = time_effect
-        self.rtime_effect = rtime_effect
+class PFAplus(ModelC): 
 
-    def __str__(self):
-        return  "Elo " + `self.alpha` + ' '+`self.time_effect` + ' '+ `self.rtime_effect`
-        
-    def process_sequence(self, initskill, ans, qtype, time, rtime):
-        skill = initskill
-        skill += self.alpha * (ans[0] - sigmoid_shift(skill, random_factor(qtype[0])))
-        for i in range(1, len(ans)):
-            timedif = max(0, time[i]-time[i-1])
-            local_skill = skill + float(self.time_effect) / ( 1 + timedif )
-            pred = sigmoid_shift(local_skill, random_factor(qtype[i]))
-            self.save_to_log(pred, ans[i])
-            
-            alpha = self.alpha * (1 + (RTIME_MEAN - rtime[i]) * self.rtime_effect)
-            skill += alpha * (ans[i] - pred)
-            
-class EloBasic(Elo):
-    def __init__(self, alpha = 2.0):
-        Elo.__init__(self, alpha, 0)
-        
-class PFA(ModelC): 
-
-#    def __init__(self, Kgood = 1.0, Kbad = 0.8, time_effect = 160):  # opt. setting for data with mintimestep 600
-#    def __init__(self, Kgood = 0.4, Kbad = -0.6, time_effect = 60): # rmse2
-    def __init__(self, Kgood = 0.8, Kbad = 0.2, time_effect = 60, rtime_effect = 0):
+    def __init__(self, Kgood = 3.4, Kbad = -0.2, time_effect = 80, rtime_effect = 0.25, use_prob = 1):
+        self.use_prob = use_prob
         self.Kgood = Kgood
         self.Kbad = Kbad
         self.time_effect = time_effect
         self.rtime_effect = rtime_effect
 
     def __str__(self):
-        return  "PFA " + `self.Kgood` + " " + `self.Kbad` + " " + `self.time_effect` +  ' '+ `self.rtime_effect`
+        return  "PFAplus(" + ",".join(map(str,[self.use_prob, self.Kgood, self.Kbad, self.time_effect,self.rtime_effect])) + ")"
         
     def process_sequence(self, initskill, ans, qtype, time, rtime):
         skill = initskill
@@ -110,34 +89,45 @@ class PFA(ModelC):
                 timedif = max(0, time[i]-time[i-1])
                 local_skill += float(self.time_effect) / ( 1 + timedif )
             pred = sigmoid_shift(local_skill, random_factor(qtype[i]))
+            
             if i > 0: self.save_to_log(pred, ans[i])
-            if ans[i]: skill += self.Kgood * (1 + (RTIME_MEAN - rtime[i]) * self.rtime_effect)
-            else: skill += self.Kbad
-
-class PFAElo(ModelC): 
-
-    def __init__(self, Kgood = 3.0, Kbad = 0.2, time_effect = 80):
-        self.Kgood = Kgood
-        self.Kbad = Kbad
-        self.time_effect = time_effect
-
-    def __str__(self):
-        return  "PFAElo " + `self.Kgood` + " " + `self.Kbad` + " " + `self.time_effect`
-        
-    def process_sequence(self, initskill, ans, qtype, time, rtime):
-        skill = initskill
-        for i in range(len(ans)):
-            local_skill = skill
-            if i > 0:
-                timedif = max(0, time[i]-time[i-1])
-                local_skill += float(self.time_effect) / ( 1 + timedif )
-            pred = sigmoid_shift(local_skill, random_factor(qtype[i]))
-            if i > 0: self.save_to_log(pred, ans[i])
+            
             if ans[i]:
-                skill += self.Kgood * (1 - pred)
+                K = self.Kgood
+                K = K * (1 + (RTIME_MEAN - rtime[i]) * self.rtime_effect)
+                if self.use_prob:
+                    K = K * (1-pred)
             else:
-                skill -= self.Kbad * pred
+                K = self.Kbad
+                if self.use_prob:
+                    K = K * pred
+            skill += K            
+                
+        
+class PFAtime(PFAplus): 
 
+    def __init__(self, Kgood = 0.75, Kbad = 0.2, time_effect = 90, rtime_effect = 0.3):
+        PFAplus.__init__(self, Kgood, Kbad, time_effect, rtime_effect, 0)
+
+    def __str__(self):
+        return  "PFAtime(" + ",".join(map(str,[self.use_prob, self.Kgood, self.Kbad, self.time_effect,self.rtime_effect])) + ")"
+
+class PFAbasic(PFAplus): 
+
+    def __init__(self, Kgood = 0.9, Kbad = 0.6):
+        PFAplus.__init__(self, Kgood, Kbad, 0, 0, 0)
+
+    def __str__(self):
+        return  "PFAbasic(" + ",".join(map(str,[self.use_prob, self.Kgood])) + ")"
+
+class PFAprob(PFAplus): 
+
+    def __init__(self, Kgood = 3.4, Kbad = 0.2):
+        PFAplus.__init__(self, Kgood, Kbad, 0, 0, 1)
+
+    def __str__(self):
+        return  "PFAprob(" + ",".join(map(str,[self.use_prob, self.Kgood])) + ")"
+    
             
 ########## outdated, probably poor model, not worth further extensions
 ###### hmmm.... with rmse2 quite good...
@@ -163,4 +153,31 @@ class TimeDecay(ModelC):
                 self.save_to_log(pred, ans[i])
 
 
+############ outdated .... out of favour
+                
+class Elo(ModelC):
+
+    def __init__(self, alpha = 2.0, time_effect = 160.0, rtime_effect = 0):
+        self.alpha = alpha
+        self.time_effect = time_effect
+        self.rtime_effect = rtime_effect
+
+    def __str__(self):
+        return  "Elo " + `self.alpha` + ' '+`self.time_effect` + ' '+ `self.rtime_effect`
+        
+    def process_sequence(self, initskill, ans, qtype, time, rtime):
+        skill = initskill
+        skill += self.alpha * (ans[0] - sigmoid_shift(skill, random_factor(qtype[0])))
+        for i in range(1, len(ans)):
+            timedif = max(0, time[i]-time[i-1])
+            local_skill = skill + float(self.time_effect) / ( 1 + timedif )
+            pred = sigmoid_shift(local_skill, random_factor(qtype[i]))
+            self.save_to_log(pred, ans[i])
+            
+            alpha = self.alpha * (1 + (RTIME_MEAN - rtime[i]) * self.rtime_effect)
+            skill += alpha * (ans[i] - pred)
+            
+class EloBasic(Elo):
+    def __init__(self, alpha = 2.0):
+        Elo.__init__(self, alpha, 0)
             
